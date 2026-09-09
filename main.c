a6094a377e155b9c6b38cab93e5c9247960e55c6
Glen Kulobone <225050293@nust.na>
2026-09-09 14:12:18 +0200

    added more code from lab 2

--- main.c
+++ main.c
@@ -6,6 +6,15 @@ int main()
     char mayor[50];
     int population;
 
+    double revenue;
+    double expenses;
+    double balance;
+
+    int departments;
+    double payroll;
+    double procurement;
+    double assets;
+
     printf(" MUNICIPAL FINANCIAL MANAGEMENT SYSTEM\n\n");
 
     printf("Welcome to Windhoek Municipality\n\n");
@@ -19,10 +28,44 @@ int main()
     printf(" Enter Population: ");
     scanf("%d", &population);
 
+    printf("Enter total revenue: ");
+    scanf("%lf", &revenue);
+
+    printf("Enter total expenses: ");
+    scanf("%lf", &expenses);
+
+    balance = revenue - expenses;
+
+    printf("Enter number of departments: "); 
+    scanf("%d", &departments);
+
+    printf("Enter payroll: "); 
+    scanf("%lf", &payroll);
+
+    printf("Enter procurement: "); 
+    scanf("%lf", &procurement);
+
+    printf("Enter assets: "); 
+    scanf("%lf", &assets);
+
+    
      printf("\n--------------------\n");
      printf("Municipality : %s\n", municipality);
-     printf("Mayor's      : %s\n",mayor);
+     printf("Mayor's      : %s\n", mayor);
      printf("Population   : %d\n", population);
+     
+     printf("\nMUNICIPAL BUDGET SUMMARY\n"); 
+     printf("------------------------\n"); 
+     printf("Revenue        : %.2f\n", revenue);
+     printf("Expenses       : %.2f\n", expenses); 
+     printf("Budget balance : %.2f\n", balance);
+
+     printf("\nMUNICIPAL FINANCIAL SUMMARY\n"); 
+     printf("---------------------------\n"); 
+     printf("Departments : %d\n", departments); 
+     printf("Payroll     : %.2f\n", payroll); 
+     printf("Procurement : %.2f\n", procurement); 
+     printf("Assets      : %.2f\n", assets);
 
      return 0;
 
